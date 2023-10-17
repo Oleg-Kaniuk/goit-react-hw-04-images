40840226a7c9cb316df8004f3986bdb30295f06d
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2023-10-17 18:37:48 +0300

    refactoring into hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 import { fetchPhoto, fetchError } from "./api/api";
@@ -19,30 +19,24 @@ export const styleNotify = {
   fontSize: '20px'
 };
 
-export class App extends Component {
-  state = {
-    page: 1,
-    search: '',
-    photos: [],
-    isLoading: false,
-    btnLoadMore: false,
-    showModal: false,
-    selectedPhoto: null,
-  }
+export const App = () => {
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState('');
+  const [photos, setPhotos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [btnLoadMore, setBtnLoadMore] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
 
-  componentDidUpdate = (_, prevState) => {
-    const nextPage = this.state.page;
-    const nextSearch = this.state.search;
-    const prevPage = prevState.page;
-    const prevSearch = prevState.search;
-
-    if (prevSearch !== nextSearch || prevPage !== nextPage) {
-      this.addNewPage(nextSearch, nextPage);
+useEffect(() => {
+    if (!search) {
+      return;
     };
-  }
+    addNewPage(search, page);
+  }, [search, page]);
 
-  addNewPage = (search, page) => {
-    this.setState({ isLoading: true });
+const addNewPage = (search, page) => {
+    setIsLoading(true);
 
     fetchPhoto(search, page, perPage)
       .then(data => {
@@ -56,82 +50,186 @@ export class App extends Component {
           { id, webformatURL, largeImageURL, tags }
         ));
         
-        this.setState(prevState =>
-          ({ photos: [...prevState.photos, ...photoArray] }));
+        setPhotos(prevPhotos => [...prevPhotos, ...photoArray]);
         
         if (totalPage > page) {
-          this.setState({ btnLoadMore: true })
+          setBtnLoadMore(true);
         } else {
           Notify.info("We're sorry, but you've reached the end of search results.", styleNotify);
-          this.setState({ btnLoadMore: false });
+          setBtnLoadMore(false);
         };
       })
       .catch(fetchError)
-      .finally(() => {
-        this.setState({ isLoading: false });
-      });
+      .finally(() => setIsLoading(false));
   };
-  
-  onSubmitSearch = (evt) => {
+
+const onSubmitSearch = (evt) => {
     evt.preventDefault();
     const form = evt.currentTarget;
     const searchImage = form.search.value
       .trim()
       .toLowerCase();
-      
     
     if (searchImage === '') {
       Notify.info('Enter your request, please!', styleNotify);
       return;
     };
 
-    if (searchImage === this.state.search) {
+    if (searchImage === search) {
       Notify.info('Enter new request, please!', styleNotify);
       return;
     };
 
-    this.setState({
-      search: searchImage,
-      page: 1,
-      photos: [],
-    });
+    setSearch(searchImage);
+    setPage(1);
+    setPhotos([]);
+  };
+
+const toggleModal = () => {
+    setShowModal(prevShowModal => !prevShowModal);
+  };
+
+const onClickOpenModal = (evt) => {
+    const imageId = evt.target.getAttribute('data-id');
+    const selectedLargePhoto = photos.find(photo =>
+      photo.id === Number(imageId));
+    setSelectedPhoto(selectedLargePhoto);
+
+    toggleModal();
   }
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal
-    }));
-  }
+const onClickReloading = () => {
+    setPage(prevPage => prevPage + 1);
+  };
 
-  onClickOpenModal = (evt) => {
-    const { photos } = this.state;
-    const idImage = evt.target.getAttribute('data-id');
-    const selectedPhoto = photos.find(photo =>
-      photo.id === Number(idImage));
-    this.setState({ selectedPhoto });
-    this.toggleModal();
-  }
-
-  onClickReloading = () => {
-    this.setState(({ page }) => ({
-      page: page + 1
-    }));
-  }
-
-  render() {
-    const { photos, btnLoadMore, isLoading, showModal, selectedPhoto } = this.state;
-    
-    return (
+return (
       <Container>
         <Title>Image finder</Title>
-        <Searchbar onSubmitSearch={this.onSubmitSearch} />
+        <Searchbar onSubmitSearch={onSubmitSearch} />
         {isLoading && <Loader />}
         <AppContainer>
-          <ImageGallery photos={photos} onClickImage={this.onClickOpenModal} />;
+          <ImageGallery photos={photos} onClickImage={onClickOpenModal} />;
         </AppContainer>
-        {photos.length !== 0 && btnLoadMore && <Button onClickReloading={this.onClickReloading} />}
-        {showModal && <Modal selectedPhoto={selectedPhoto} onClose={this.toggleModal} />}
+        {photos.length !== 0 && btnLoadMore && <Button onClickReloading={onClickReloading} />}
+        {showModal && <Modal selectedPhoto={selectedPhoto} onClose={toggleModal} />}
       </Container>
     );
-  }
-};
+
+}
+
+// export class App extends Component {
+//   state = {
+//     page: 1,
+//     search: '',
+//     photos: [],
+//     isLoading: false,
+//     btnLoadMore: false,
+//     showModal: false,
+//     selectedPhoto: null,
+//   }
+
+//   componentDidUpdate = (_, prevState) => {
+//     const nextPage = this.state.page;
+//     const nextSearch = this.state.search;
+//     const prevPage = prevState.page;
+//     const prevSearch = prevState.search;
+
+//     if (prevSearch !== nextSearch || prevPage !== nextPage) {
+//       this.addNewPage(nextSearch, nextPage);
+//     };
+//   }
+
+//   addNewPage = (search, page) => {
+//     this.setState({ isLoading: true });
+
+//     fetchPhoto(search, page, perPage)
+//       .then(data => {
+//         const { totalHits } = data;
+//         const totalPage = Math.ceil(data.totalHits / perPage);
+//         if (totalHits === 0) {
+//           return Notify.failure('Sorry, there are no images matching your search query. Please try again.', styleNotify);
+//         }
+
+//         const photoArray = data.hits.map(({ id, webformatURL, largeImageURL, tags }) => (
+//           { id, webformatURL, largeImageURL, tags }
+//         ));
+        
+//         this.setState(prevState =>
+//           ({ photos: [...prevState.photos, ...photoArray] }));
+        
+//         if (totalPage > page) {
+//           this.setState({ btnLoadMore: true })
+//         } else {
+//           Notify.info("We're sorry, but you've reached the end of search results.", styleNotify);
+//           this.setState({ btnLoadMore: false });
+//         };
+//       })
+//       .catch(fetchError)
+//       .finally(() => {
+//         this.setState({ isLoading: false });
+//       });
+//   };
+  
+//   onSubmitSearch = (evt) => {
+//     evt.preventDefault();
+//     const form = evt.currentTarget;
+//     const searchImage = form.search.value
+//       .trim()
+//       .toLowerCase();
+      
+    
+//     if (searchImage === '') {
+//       Notify.info('Enter your request, please!', styleNotify);
+//       return;
+//     };
+
+//     if (searchImage === this.state.search) {
+//       Notify.info('Enter new request, please!', styleNotify);
+//       return;
+//     };
+
+//     this.setState({
+//       search: searchImage,
+//       page: 1,
+//       photos: [],
+//     });
+//   }
+
+//   toggleModal = () => {
+//     this.setState(({ showModal }) => ({
+//       showModal: !showModal
+//     }));
+//   }
+
+//   onClickOpenModal = (evt) => {
+//     const { photos } = this.state;
+//     const idImage = evt.target.getAttribute('data-id');
+//     const selectedPhoto = photos.find(photo =>
+//       photo.id === Number(idImage));
+//     this.setState({ selectedPhoto });
+//     this.toggleModal();
+//   }
+
+//   onClickReloading = () => {
+//     this.setState(({ page }) => ({
+//       page: page + 1
+//     }));
+//   }
+
+//   render() {
+//     const { photos, btnLoadMore, isLoading, showModal, selectedPhoto } = this.state;
+    
+//     return (
+//       <Container>
+//         <Title>Image finder</Title>
+//         <Searchbar onSubmitSearch={this.onSubmitSearch} />
+//         {isLoading && <Loader />}
+//         <AppContainer>
+//           <ImageGallery photos={photos} onClickImage={this.onClickOpenModal} />;
+//         </AppContainer>
+//         {photos.length !== 0 && btnLoadMore && <Button onClickReloading={this.onClickReloading} />}
+//         {showModal && <Modal selectedPhoto={selectedPhoto} onClose={this.toggleModal} />}
+//       </Container>
+//     );
+//   }
+// };
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,40 +1,76 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { Overlay, ModalWindow } from './Modal.styled';
 
 const rootModal = document.querySelector('#root-modal');
 
-export class Modal extends Component {
+export const Modal = ({ selectedPhoto: { largeImageURL, tags }, onClose }) => {
+    
+    useEffect(() => {
+        window.addEventListener('keydown', onEscapeCloseModal);
+        return () => {
+            window.removeEventListener('keydown', onEscapeCloseModal);
+        };
+    }, [onEscapeCloseModal]);
 
-    componentDidMount = () => {
-        window.addEventListener('keydown', this.onEscapeCloseModal);
-    }
-
-    componentWillUnmount = () => {
-        window.removeEventListener('keydown', this.onEscapeCloseModal);
-    }
-
-    onEscapeCloseModal = (evt) => {
+    function onEscapeCloseModal(evt) {
         if (evt.code === 'Escape') {
-            this.props.onClose()
-        }
+            onClose();
+        };
     }
 
-    onClickOverlay = (evt) => {
+    const onClickOverlay = evt => {
         if (evt.target === evt.currentTarget) {
-            this.props.onClose()
-        }
+            onClose();
+        };
     }
 
-    render() {
-        const { selectedPhoto: { largeImageURL, tags } } = this.props;
-
-        return createPortal(
-            <Overlay onClick={this.onClickOverlay}>
+    return createPortal(
+            <Overlay onClick={onClickOverlay}>
                 <ModalWindow>
                     <img src={largeImageURL} alt={tags} />
                 </ModalWindow>
             </Overlay>,
             rootModal);
-    }
 }
+
+
+
+
+
+
+
+// export class Modal extends Component {
+
+//     componentDidMount = () => {
+//         window.addEventListener('keydown', this.onEscapeCloseModal);
+//     }
+
+//     componentWillUnmount = () => {
+//         window.removeEventListener('keydown', this.onEscapeCloseModal);
+//     }
+
+//     onEscapeCloseModal = (evt) => {
+//         if (evt.code === 'Escape') {
+//             this.props.onClose()
+//         }
+//     }
+
+//     onClickOverlay = (evt) => {
+//         if (evt.target === evt.currentTarget) {
+//             this.props.onClose()
+//         }
+//     }
+
+//     render() {
+//         const { selectedPhoto: { largeImageURL, tags } } = this.props;
+
+//         return createPortal(
+//             <Overlay onClick={this.onClickOverlay}>
+//                 <ModalWindow>
+//                     <img src={largeImageURL} alt={tags} />
+//                 </ModalWindow>
+//             </Overlay>,
+//             rootModal);
+//     }
+// }
