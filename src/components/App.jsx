95a5dde65ee0736ea470c930b06854f1beabca6b
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2023-10-21 19:40:16 +0300

    comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,28 @@
+// Імпортуючи useState та useEffect з бібліотеки React для управління станом та використання ефектів
 import { useState, useEffect } from "react";
+
+// Імпортуючи компонент Notify з бібліотеки Notiflix для відображення повідомлень
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+// Імпортуючи функції для взаємодії з API
 import { fetchPhoto, fetchError } from "./api/api";
+
+// Імпортуючи стилізовані компоненти для головного контейнера, назви та внутрішнього контейнера
 import { AppContainer, Title, Container } from "./App.styled";
 
+// Імпортуючи компоненти для пошукової стрічки, галереї зображень, кнопки та завантажувача
 import { Searchbar } from "./Searchbar/Searchbar";
 import { ImageGallery } from "./ImageGallery/ImageGallery";
 import { Button } from "./Button/Button";
 import { Loader } from "./Loader/Loader";
+
+// Імпортуючи компонент Modal для відображення зображення в модальному вікні
 import { Modal } from "./Modal/Modal";
 
+// Кількість зображень, які відображаються на сторінці
 const perPage = 12;
 
+// Стиль для відображення повідомлень Notiflix
 export const styleNotify = {
   position: 'top-right',
   timeout: 2000,
@@ -19,7 +30,9 @@ export const styleNotify = {
   fontSize: '20px'
 };
 
+// Визначення головного компоненту App
 export const App = () => {
+  // Використання useState для управління станом сторінки, рядка пошуку, списку зображень, вказує, чи треба відображати кнопку "Load More", та стану модального вікна
   const [page, setPage] = useState(1);
   const [search, setSearch] = useState('');
   const [photos, setPhotos] = useState([]);
@@ -28,14 +41,16 @@ export const App = () => {
   const [showModal, setShowModal] = useState(false);
   const [selectedPhoto, setSelectedPhoto] = useState(null);
 
-useEffect(() => {
+  // Ефект для виклику нової сторінки зображень при зміні рядка пошуку чи номера сторінки
+  useEffect(() => {
     if (!search) {
       return;
-    };
+    }
     addNewPage(search, page);
   }, [search, page]);
 
-const addNewPage = (search, page) => {
+  // Функція для отримання нової сторінки зображень та її відображення
+  const addNewPage = (search, page) => {
     setIsLoading(true);
 
     fetchPhoto(search, page, perPage)
@@ -43,18 +58,23 @@ const addNewPage = (search, page) => {
         const { totalHits } = data;
         const totalPage = Math.ceil(data.totalHits / perPage);
         if (totalHits === 0) {
+          // Виведення повідомлення про відсутність зображень за вказаним запитом
           return Notify.failure('Sorry, there are no images matching your search query. Please try again.', styleNotify);
         }
 
+        // Створення масиву зображень з отриманих даних
         const photoArray = data.hits.map(({ id, webformatURL, largeImageURL, tags }) => (
           { id, webformatURL, largeImageURL, tags }
         ));
-        
+
+        // Додавання нових зображень до списку
         setPhotos(prevPhotos => [...prevPhotos, ...photoArray]);
-        
+
+        // Визначення, чи треба відображати кнопку "Load More"
         if (totalPage > page) {
           setBtnLoadMore(true);
         } else {
+          // Виведення повідомлення про досягнення кінця результатів пошуку
           Notify.info("We're sorry, but you've reached the end of search results.", styleNotify);
           setBtnLoadMore(false);
         };
@@ -63,173 +83,72 @@ const addNewPage = (search, page) => {
       .finally(() => setIsLoading(false));
   };
 
-const onSubmitSearch = (evt) => {
+  // Функція для обробки відправлення форми пошукового запиту
+  const onSubmitSearch = (evt) => {
     evt.preventDefault();
     const form = evt.currentTarget;
     const searchImage = form.search.value
       .trim()
       .toLowerCase();
-    
+
+    // Перевірка наявності тексту у полі пошуку
     if (searchImage === '') {
+      // Виведення повідомлення про необхідність введення запиту
       Notify.info('Enter your request, please!', styleNotify);
       return;
-    };
+    }
 
+    // Перевірка на зміну пошукового запиту
     if (searchImage === search) {
+      // Виведення повідомлення про необхідність введення нового запиту
       Notify.info('Enter new request, please!', styleNotify);
       return;
-    };
+    }
 
+    // Встановлення нового рядка пошуку та обнулення списку зображень
     setSearch(searchImage);
     setPage(1);
     setPhotos([]);
   };
 
-const toggleModal = () => {
+  // Функція для зміни стану відображення модального вікна
+  const toggleModal = () => {
     setShowModal(prevShowModal => !prevShowModal);
   };
 
-const onClickOpenModal = (evt) => {
+  // Функція для відкриття модального вікна та вибору обраного зображення
+  const onClickOpenModal = (evt) => {
     const imageId = evt.target.getAttribute('data-id');
     const selectedLargePhoto = photos.find(photo =>
       photo.id === Number(imageId));
     setSelectedPhoto(selectedLargePhoto);
 
     toggleModal();
-  }
+  };
 
-const onClickReloading = () => {
+  // Функція для завантаження наступної сторінки зображень
+  const onClickReloading = () => {
     setPage(prevPage => prevPage + 1);
   };
 
-return (
-      <Container>
-        <Title>Image finder</Title>
-        <Searchbar onSubmitSearch={onSubmitSearch} />
-        {isLoading && <Loader />}
-        <AppContainer>
-          <ImageGallery photos={photos} onClickImage={onClickOpenModal} />;
-        </AppContainer>
-        {photos.length !== 0 && btnLoadMore && <Button onClickReloading={onClickReloading} />}
-        {showModal && <Modal selectedPhoto={selectedPhoto} onClose={toggleModal} />}
-      </Container>
-    );
+
+// Відображення головного контейнера додатку
+  return (
+    <Container>
+      <Title>Image finder</Title>
+      {/* Виклик компонента пошукової стрічки та передача функції для обробки події відправлення форми */}
+      <Searchbar onSubmitSearch={onSubmitSearch} />
+      {/* Відображення завантажувача під час завантаження зображень */}
+      {isLoading && <Loader />}
+      <AppContainer>
+        {/* Виклик компонента галереї зображень та передача списку зображень та функції для відкриття модального вікна */}
+        <ImageGallery photos={photos} onClickImage={onClickOpenModal} />;
+      </AppContainer>
+      {/* Відображення кнопки "Load More", якщо є результати та ще не досягнуто кінця результатів пошуку */}
+      {photos.length !== 0 && btnLoadMore && <Button onClickReloading={onClickReloading} />}
+      {/* Відображення модального вікна, якщо воно відкрите */}
+      {showModal && <Modal selectedPhoto={selectedPhoto} onClose={toggleModal} />}
+    </Container>
+  );
 
 }
-
-// export class App extends Component {
-//   state = {
-//     page: 1,
-//     search: '',
-//     photos: [],
-//     isLoading: false,
-//     btnLoadMore: false,
-//     showModal: false,
-//     selectedPhoto: null,
-//   }
-
-//   componentDidUpdate = (_, prevState) => {
-//     const nextPage = this.state.page;
-//     const nextSearch = this.state.search;
-//     const prevPage = prevState.page;
-//     const prevSearch = prevState.search;
-
-//     if (prevSearch !== nextSearch || prevPage !== nextPage) {
-//       this.addNewPage(nextSearch, nextPage);
-//     };
-//   }
-
-//   addNewPage = (search, page) => {
-//     this.setState({ isLoading: true });
-
-//     fetchPhoto(search, page, perPage)
-//       .then(data => {
-//         const { totalHits } = data;
-//         const totalPage = Math.ceil(data.totalHits / perPage);
-//         if (totalHits === 0) {
-//           return Notify.failure('Sorry, there are no images matching your search query. Please try again.', styleNotify);
-//         }
-
-//         const photoArray = data.hits.map(({ id, webformatURL, largeImageURL, tags }) => (
-//           { id, webformatURL, largeImageURL, tags }
-//         ));
-        
-//         this.setState(prevState =>
-//           ({ photos: [...prevState.photos, ...photoArray] }));
-        
-//         if (totalPage > page) {
-//           this.setState({ btnLoadMore: true })
-//         } else {
-//           Notify.info("We're sorry, but you've reached the end of search results.", styleNotify);
-//           this.setState({ btnLoadMore: false });
-//         };
-//       })
-//       .catch(fetchError)
-//       .finally(() => {
-//         this.setState({ isLoading: false });
-//       });
-//   };
-  
-//   onSubmitSearch = (evt) => {
-//     evt.preventDefault();
-//     const form = evt.currentTarget;
-//     const searchImage = form.search.value
-//       .trim()
-//       .toLowerCase();
-      
-    
-//     if (searchImage === '') {
-//       Notify.info('Enter your request, please!', styleNotify);
-//       return;
-//     };
-
-//     if (searchImage === this.state.search) {
-//       Notify.info('Enter new request, please!', styleNotify);
-//       return;
-//     };
-
-//     this.setState({
-//       search: searchImage,
-//       page: 1,
-//       photos: [],
-//     });
-//   }
-
-//   toggleModal = () => {
-//     this.setState(({ showModal }) => ({
-//       showModal: !showModal
-//     }));
-//   }
-
-//   onClickOpenModal = (evt) => {
-//     const { photos } = this.state;
-//     const idImage = evt.target.getAttribute('data-id');
-//     const selectedPhoto = photos.find(photo =>
-//       photo.id === Number(idImage));
-//     this.setState({ selectedPhoto });
-//     this.toggleModal();
-//   }
-
-//   onClickReloading = () => {
-//     this.setState(({ page }) => ({
-//       page: page + 1
-//     }));
-//   }
-
-//   render() {
-//     const { photos, btnLoadMore, isLoading, showModal, selectedPhoto } = this.state;
-    
-//     return (
-//       <Container>
-//         <Title>Image finder</Title>
-//         <Searchbar onSubmitSearch={this.onSubmitSearch} />
-//         {isLoading && <Loader />}
-//         <AppContainer>
-//           <ImageGallery photos={photos} onClickImage={this.onClickOpenModal} />;
-//         </AppContainer>
-//         {photos.length !== 0 && btnLoadMore && <Button onClickReloading={this.onClickReloading} />}
-//         {showModal && <Modal selectedPhoto={selectedPhoto} onClose={this.toggleModal} />}
-//       </Container>
-//     );
-//   }
-// };
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,17 +1,26 @@
+// Імпортуючи useEffect та useCallback з бібліотеки React для використання побічних ефектів та оптимізації функцій
 import { useEffect, useCallback } from 'react';
+
+// Імпортуючи функцію createPortal з react-dom для відображення компоненту поза його звичайним DOM-контекстом
 import { createPortal } from 'react-dom';
+
+// Імпортуючи стилізовані компоненти для модального вікна
 import { Overlay, ModalWindow } from './Modal.styled';
 
+// Визначення точки монтажу для модального вікна
 const rootModal = document.querySelector('#root-modal');
 
+// Визначення компоненту Modal
 export const Modal = ({ selectedPhoto: { largeImageURL, tags }, onClose }) => {
 
+    // Функція для закриття модального вікна при натисканні клавіші 'Escape'
     const onEscapeCloseModal = useCallback((evt) => {
         if (evt.code === 'Escape') {
             onClose();
         }
     }, [onClose]);
 
+    // Ефект для додавання слухача клавіші 'Escape' при монтажі компонента та його видалення при демонтажі
     useEffect(() => {
         const handleEscape = (evt) => onEscapeCloseModal(evt);
         window.addEventListener('keydown', handleEscape);
@@ -20,60 +29,22 @@ export const Modal = ({ selectedPhoto: { largeImageURL, tags }, onClose }) => {
         };
     }, [onEscapeCloseModal]);
 
+    // Функція для закриття модального вікна при кліку на затемнений фон (overlay)
     const onClickOverlay = useCallback((evt) => {
         if (evt.target === evt.currentTarget) {
             onClose();
         }
     }, [onClose]);
 
+    // Використання createPortal для відображення компонента за межами його звичайного DOM-контексту
     return createPortal(
+        // Оверлей та модальне вікно зображення
         <Overlay onClick={onClickOverlay}>
             <ModalWindow>
                 <img src={largeImageURL} alt={tags} />
             </ModalWindow>
         </Overlay>,
+        // Кореневий елемент для монтажу модального вікна
         rootModal
     );
 };
-
-
-
-
-
-
-
-
-// export class Modal extends Component {
-
-//     componentDidMount = () => {
-//         window.addEventListener('keydown', this.onEscapeCloseModal);
-//     }
-
-//     componentWillUnmount = () => {
-//         window.removeEventListener('keydown', this.onEscapeCloseModal);
-//     }
-
-//     onEscapeCloseModal = (evt) => {
-//         if (evt.code === 'Escape') {
-//             this.props.onClose()
-//         }
-//     }
-
-//     onClickOverlay = (evt) => {
-//         if (evt.target === evt.currentTarget) {
-//             this.props.onClose()
-//         }
-//     }
-
-//     render() {
-//         const { selectedPhoto: { largeImageURL, tags } } = this.props;
-
-//         return createPortal(
-//             <Overlay onClick={this.onClickOverlay}>
-//                 <ModalWindow>
-//                     <img src={largeImageURL} alt={tags} />
-//                 </ModalWindow>
-//             </Overlay>,
-//             rootModal);
-//     }
-// }
